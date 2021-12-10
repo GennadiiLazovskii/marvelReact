a961216729f1f3d95a8e227d74a1e9add1d118bd
Gennadiy <lozgen@ukr.net>
2021-12-10 15:56:42 +0200

    implementation of random heroes and implementation of loading and errors

--- marvel_starter/src/services/MarvelService.js
+++ marvel_starter/src/services/MarvelService.js
@@ -14,12 +14,24 @@
       return await res.json();
     }
 
-    getAllCharacters = () => {
-      return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apikey}`);
+    getAllCharacters = async () => {
+      const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apikey}`);
+      return res.data.results.map(this._transformCharacter);
     }
 
-    getCharacter = (id) => {
-      return this.getResource(`${this._apiBase}characters/${id}?${this._apikey}`);
+    getCharacter = async (id) => {
+      const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apikey}`);
+      return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+      return {
+        name: char.name,
+        description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+        thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+        homepage: char.urls[0].url,
+        wiki: char.urls[1].url
+      }
     }
   }
 
