a2aebbd02ea5d9f011f0cb5498c078fde46c9ce1
Gennadiy <lozgen@ukr.net>
2021-12-10 11:57:06 +0200

    First Commit

---
+++ marvel_starter/src/services/MarvelService.js
@@ -0,0 +1,26 @@
+
+
+  class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apikey = 'apikey=99644365edf4734f10af4bd66e65f5ce';
+
+    getResource = async (url) => {
+      let res = await fetch(url);
+
+      if(!res.ok) {
+        throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+      }
+
+      return await res.json();
+    }
+
+    getAllCharacters = () => {
+      return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apikey}`);
+    }
+
+    getCharacter = (id) => {
+      return this.getResource(`${this._apiBase}characters/${id}?${this._apikey}`);
+    }
+  }
+
+  export default MarvelService;
