4b3f00196ad31c1fbb9c978dea5876a14fd71ba3
Gennadiy <lozgen@ukr.net>
2021-12-25 21:23:08 +0200

    FIX

--- marvel_starter/src/components/charInfo/CharInfo.js
+++ marvel_starter/src/components/charInfo/CharInfo.js
@@ -1,97 +1,86 @@
-import { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
-import useMarvelService from '../../services/MarvelService';
-import setContent from '../utils/setContent';
+import useMarvelService from "../../services/MarvelService";
+import setContent from "../utils/setContent";
 
-
-import './charInfo.scss';
+import "./charInfo.scss";
 
 const CharInfo = (props) => {
-
   const [char, setChar] = useState(null);
 
-  const {getCharacter, clearError, process, setProcess} = useMarvelService();
-
+  const { getCharacter, clearError, process, setProcess } = useMarvelService();
 
   useEffect(() => {
     updateChar();
   }, [props.charId]);
 
   const updateChar = () => {
-    const {charId} = props;
+    const { charId } = props;
     if (!charId) {
       return;
     }
 
     clearError();
     getCharacter(charId)
-        .then(onCharLoaded)
-        .then(() => setProcess('confirmed'))
-  }
+      .then(onCharLoaded)
+      .then(() => setProcess("confirmed"));
+  };
 
   const onCharLoaded = (char) => {
     setChar(char);
-  }
+  };
 
-    return (
-      <div className="char__info">
-          {setContent(process, View, char)}
-      </div>
-  )
-}
+  return <div className="char__info">{setContent(process, View, char)}</div>;
+};
 
-const View = ({data}) => {
-  const {name, description, thumbnail, homepage, wiki, comics} = data;
+const View = ({ data }) => {
+  const { name, description, thumbnail, homepage, wiki, comics } = data;
 
-  let imgStyle = {'objectFit': 'cover'};
-  if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
-    imgStyle = {'objectFit': 'contain'};
+  let imgStyle = { objectFit: "cover" };
+  if (
+    thumbnail ===
+    "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+  ) {
+    imgStyle = { objectFit: "contain" };
   }
 
   return (
     <>
       <div className="char__basics">
-                <img src={thumbnail} alt={name} style={imgStyle}/>
-                <div>
-                    <div className="char__info-name">{name}</div>
-                    <div className="char__btns">
-                        <a href={homepage} className="button button__main">
-                            <div className="inner">homepage</div>
-                        </a>
-                        <a href={wiki} className="button button__secondary">
-                            <div className="inner">Wiki</div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div className="char__descr">
-                {description}
-            </div>
-            <div className="char__comics">Comics:</div>
-            <ul className="char__comics-list">
+        <img src={thumbnail} alt={name} style={imgStyle} />
+        <div>
+          <div className="char__info-name">{name}</div>
+          <div className="char__btns">
+            <a href={homepage} className="button button__main">
+              <div className="inner">homepage</div>
+            </a>
+            <a href={wiki} className="button button__secondary">
+              <div className="inner">Wiki</div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="char__descr">{description}</div>
+      <div className="char__comics">Comics:</div>
+      <ul className="char__comics-list">
+        {comics.length > 0 ? null : "There is not comics with this characters"}
 
-              {comics.length > 0 ? null : 'There is not comics with this characters'}
-
-              {
-                comics.map((item, i) => {
-
-                  if (i > 9) return;
-                  return (
-                    <li key={i} className="char__comics-item">
-                      {item.name}
-                    </li>
-                  )
-                })
-              }
-            </ul>
+        {comics.map((item, i) => {
+          if (i > 9) return;
+          return (
+            <li key={i} className="char__comics-item">
+              {item.name}
+            </li>
+          );
+        })}
+      </ul>
     </>
-  )
-}
+  );
+};
 
 CharInfo.propTypes = {
-  charId: PropTypes.number
-}
+  charId: PropTypes.number,
+};
 
 export default CharInfo;
